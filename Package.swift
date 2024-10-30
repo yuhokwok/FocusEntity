2c46fbbb5851ea902faf8c2f66c246be89e1b2d4
Will Kwok <yuho1985@gmail.com>
2024-10-30 11:29:05 +0800

    revised package.swift

--- Package.swift
+++ Package.swift
@@ -5,7 +5,7 @@ import PackageDescription
 
 let package = Package(
     name: "FocusEntity",
-    platforms: [.iOS(.v13), .macOS(.v10_15)],
+    platforms: [.iOS(.v15), .macOS(.v12)],
     products: [
         .library(name: "FocusEntity", targets: ["FocusEntity"])
     ],
